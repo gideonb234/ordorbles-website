e32cd5652a7b83f0f4fe74fda44c092144ecd21a
Gideon <gideonb234@gmail.com>
2015-04-25 12:30:57 +0100

    static controller and index page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,11 +11,6 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::get('/', 'StaticController@index');
 
-//Route::get('home', 'HomeController@index');
-
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+Route::get('contact', 'StaticController@contact');
---
+++ resources/views/static/index.blade.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet"></link>
+    <title></title>
+</head>
+<body>
+<div id="container">
+    <div class="header">
+        <nav class="navbar navbar-default">
+            <div class="container-fluid"></div>
+                <div class="navbar-header">
+                    <p class="navbar-brand">Ordorbles</p>
+                </div>
+        </nav>
+    </div>
+    <p></p>
+</div>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</body>
+
